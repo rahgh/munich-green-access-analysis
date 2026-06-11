239f85b005be87e2989f36c5d878cc6749f30449
Rahimeh Gh <gharibpour.rah@gmail.com>
2026-06-11 11:14:11 +0200

    Initial project structure

---
+++ scripts/test_connection.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+engine = create_engine(
+    "postgresql://postgres:{password}@localhost/munich_green"
+)
+
+query = "SELECT PostGIS_Version();"
+
+df = pd.read_sql(query, engine)
+
+print(df)
